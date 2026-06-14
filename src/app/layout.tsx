6f050dde0492c1bf87fc593790445749cd7d0d76
Bruno Fukumori <brunofukumori@gmail.com>
2026-06-14 16:26:25 -0300

    feat: add observability

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -20,7 +22,11 @@ export default function RootLayout({
       lang="pt-BR"
       className={`dark ${inter.variable} antialiased bg-zinc-950`}
     >
-      <body>{children}</body>
+      <body>
+        {children}
+        <SpeedInsights />
+        <Analytics />
+      </body>
     </html>
   );
 }
